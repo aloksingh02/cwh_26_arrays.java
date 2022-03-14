fa322e1b664aa3b7f967354997e52f202d5a3c46
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-03-14 23:58:10 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] marks = new int[5];
+        marks[0] = 100;
+        marks[1] = 15;
+        marks[2] = 180;
+        marks[3] = 170;
+        marks[4] = 109;
+        System.out.println(marks[4]);
+
+    }
+}
